fa7c209bacdab3b86b1e14a792acb90124a0a275
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-08-24 17:10:45 +0530

    added server auto-ping feature

--- src/global/constants.js
+++ src/global/constants.js
@@ -7,6 +7,7 @@ const env = {
   GROQ_API_KEY: process.env.GROQ_API_KEY,
   SYSTEM_PROMPT: process.env.SYSTEM_PROMPT,
   GROQ_MODEL_NAME: process.env.GROQ_MODEL_NAME,
+  SERVER_URL: process.env.SERVER_URL,
 };
 
 for (const [key, value] of Object.entries(env)) {
