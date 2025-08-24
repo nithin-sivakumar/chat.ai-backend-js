b65a7f290bd498f8d2fb727fdf6c1065b0563f95
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-08-24 17:33:35 +0530

    whitelisted prod FE url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://chat-ai-one-chi.vercel.app/",
+    ],
   })
 );
 
